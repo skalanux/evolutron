4c06db0daf3aac848e76c614f6244983743c4642
Felipe Batista <felipe@taniquetil.com.ar>
2023-03-24 11:00:51 -0300

    Empezando eliminacion de plantas

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import arcade
 from random import randint
+import time
 
 class Juego(arcade.Window):
     def __init__(self, width, heigth, title):
         super().__init__(width, heigth, title, fullscreen=True)
         arcade.set_background_color(arcade.color.EERIE_BLACK)
+
+        self.vegetacion_ubicacion=[]
+        self.start_time=time.time()
 
     def iniciar_pantalla(self):
         arcade.draw_text("Winter", 850, 985, arcade.color.AQUA, 50)
@@ -17,14 +21,22 @@ class Juego(arcade.Window):
             punto_y=randint(380,900)
             punto_x=randint(00,1900)
             vegetacion += 1
+            self.vegetacion_ubicacion.append((punto_x,punto_y))
             arcade.draw_rectangle_filled(punto_x, punto_y, 30, 60, arcade.color.GREEN)
+
+    def eliminar_vegetacion(self):
+        pass
+
+    def on_update(self,delta_time):
+        print(delta_time)
 
 
 
 pantalla=Juego(1920,1080, 'Evolutron')
 arcade.start_render()
 pantalla.iniciar_pantalla()
-pantalla.draw_vegetacion(600)
+pantalla.draw_vegetacion(40)
+print(pantalla.vegetacion_ubicacion)
 arcade.finish_render()
 arcade.run()
         
