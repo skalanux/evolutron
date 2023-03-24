3ced11fc54e2075258c934ad6bb576907844e731
Felipe Batista <cabra@Caroline.lan>
2023-03-23 21:05:09 -0300

    agrago franja rosa

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ class Juego(arcade.Window):
         super().__init__(width, heigth, title, fullscreen=True)
         arcade.set_background_color(arcade.color.EERIE_BLACK)
 
+    def iniciar_pantalla(self):
+        arcade.start_render()
+        arcade.draw_text("draw_filled_rect", 363, 3, arcade.color.WHITE, 10)
+        arcade.draw_rectangle_filled(960, 540, 1920, 600, arcade.color.BLUSH)
+        #arcade.draw_rectangle_filled(420, 160, 20, 40, arcade.color.BLUSH, 45)
+        arcade.finish_render()
 
 
 
 
-
-Juego(1920,1080, 'Evolutron')
+pantalla=Juego(1920,1080, 'Evolutron')
+pantalla.iniciar_pantalla()
 arcade.run()
         
